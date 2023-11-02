cf45af431aaaa0ff213ea33b560f60350e9fff0f
Lucas <l.marguiron@extralagence.com>
2023-11-02 12:42:03 +0100

    landing

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import './style.scss'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const circle = document.querySelector('.js-circle');
+const flag = document.querySelector('.js-flag');
+
+gsap.to(circle, {
+    yPercent: 100,
+    ease: "none",
+    scrollTrigger: {
+        scrub: true
+    },
+});
+
+gsap.to(flag, {
+    scale: 4,
+    ease: "none",
+    scrollTrigger: {
+        scrub: true
+    },
+});
